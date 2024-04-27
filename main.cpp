3dbb1dda2fad41e010c190d1bbd0cfffd446d764
SzauterAbel <abel.szauter@gmail.com>
2024-04-27 19:13:41 +0200

    Make the file reader function be able to run on multiple thread

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <queue>
 #include <fstream>
+#include <future>
 #include "date.hpp"
 
 typedef int (*DateProcessor)(const Date&);
@@ -18,40 +19,49 @@ auto measure_execution_time(const std::string& task_name, Func&& task) {
     std::cerr << task_name << " task was finished in " << duration << " millisec." << std::endl;
 }
 
+void read_from_start(std::ifstream *inputFile, std::vector<Date> *dateVector, int num_elements) noexcept {
+    int year, month, day;
+    for (int i = 0; i < num_elements; ++i) {
+        *inputFile >> year >> month >> day;
+        dateVector->push_back(Date(year, month, day));
+    }
+}
+
+void read_from_end(std::ifstream *inputFile,
+std::priority_queue<Date, std::vector<Date>, std::greater<Date>> *datePriorityQueue, int num_elements) noexcept {
+    int year, month, day;
+    inputFile->seekg(-sizeof(int) * num_elements, std::ios::end);
+    for (int i = 0; i < num_elements; ++i) {
+        *inputFile >> year >> month >> day;
+        datePriorityQueue->push(Date(year, month, day));
+    }
+}
+
 auto read_dates_from_file = [](std::ifstream *inputFile, int* N, int* K, std::vector<Date> *dateVector,
-    std::priority_queue<Date, std::vector<Date>, std::greater<Date>> *datePriorityQueue){
+    std::priority_queue<Date, std::vector<Date>, std::greater<Date>> *datePriorityQueue) {
         (*inputFile) >> *N >> *K;
         if (*N < 0 || *K < 0) {
             std::cerr << "Error: N and K must be non-negative" << std::endl;
             return;
         }
+        std::thread t1(read_from_start, inputFile, dateVector, *K);
+        std::thread t2(read_from_end, inputFile, datePriorityQueue, *N);
+        t1.join();
+        t2.join();
 
-        
-        for (int i = 0; i < *N; ++i) {
-            short year, month, day;
-            (*inputFile) >> year >> month >> day;
-            datePriorityQueue->push(Date(year, month, day));
-        }
-
-        for (int i = 0; i < *K; ++i) {
-            short year, month, day;
-            (*inputFile) >> year >> month >> day;
-            dateVector->push_back(Date(year, month, day));
-        }
-        (*inputFile).close();
-    };
-
+        inputFile->close();
+};
 
 auto choose_delete_replace = [](int N, int K, std::vector<Date> *dateVector,
     std::priority_queue<Date, std::vector<Date>, std::greater<Date>> *datePriorityQueue) {
     for (int i = 0; i < N + K; ++i) {
-        Date minDate = datePriorityQueue->top();
-        consumer(minDate);
-        datePriorityQueue->pop();
+            Date minDate = datePriorityQueue->top();
+            consumer(minDate);
+            datePriorityQueue->pop();
 
-        if (i < K) {
-            datePriorityQueue->push(dateVector->at(i));
-        }
+            if (i < K) {
+                datePriorityQueue->push(dateVector->at(i));
+            }
     }
 };
 
